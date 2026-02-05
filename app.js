004ccb79f1e382f6673c98958e88fb2a1c7292b7
DarkSkillz <githubskillz@gmail.com>
2026-02-05 19:04:34 +0100

    Fix bug where adding books made duplicates

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const library = []
 const bookcase = document.getElementById("bookcase")
 const addBook = document.getElementById("add-book")
+const bookForm = document.getElementById("book-form")
+const submitBtn = document.getElementById("submit")
+const form = document.getElementById("form")
 
 function Book(title, author, pages, genres, read) {
      if (!new.target) {
@@ -19,19 +22,40 @@ function addBookToLibrary(title, author, pages, genres, read) {
     library.push(newBook)
 }
 
-function addBookToCase() {
+function addBookToCase(element) {
     const book = document.createElement("div")
     const bookCover = document.createElement("div")
     const bookAuthor = document.createElement("p")
     const bookTitle = document.createElement("p")
-    book.className = "book"
+    book.className = "book removable"
     bookCover.className = "book-cover"
     bookAuthor.className = "book-author"
     bookTitle.className = "book-title"
-    bookAuthor.append("Someone somebody")
-    bookTitle.append("A very cool book indeed")
+    bookAuthor.append(String(element.author))
+    bookTitle.append(String(element.title))
     book.append(bookCover, bookAuthor, bookTitle)
     bookcase.prepend(book)
 }
 
-addBook.addEventListener("click", addBookToCase)
+addBook.addEventListener("click", () => {
+    bookForm.style.visibility = "visible"
+})
+
+document.getElementById("cancel").addEventListener("click", () => {
+    bookForm.style.visibility = "hidden"
+    form.reset()
+})
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    document.querySelectorAll(".removable").forEach((btn)=>{
+        bookcase.removeChild(btn)
+    })
+    const formData = new FormData(form)
+    const objectData = Object.fromEntries(formData)
+    bookForm.style.visibility = "hidden" //! Change to check for validity first
+    addBookToLibrary(objectData.title, objectData.author)
+    library.forEach(addBookToCase)
+    console.log(library);
+    form.reset()
+})
