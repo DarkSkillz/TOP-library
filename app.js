a60966702ba96d2fde7a46c5c7952d8739e88a20
DarkSkillz <githubskillz@gmail.com>
2026-02-09 22:00:33 +0100

    Add button to remove books and fix bug with validation forcing a choice

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const submitBtn = document.getElementById("submit")
 const form = document.getElementById("form")
 const errorMessageInfo = document.getElementById("missing-info")
 const errorMessageType = document.getElementById("invalid-type")
+const removeBookBtn = document.querySelector(".remove-book")
 
 // Book object constructor
 function Book(title, author, pages, genres, read, rating, cover) {
@@ -32,6 +33,7 @@ function addBookToLibrary(title, author, pages, genres, read, rating, cover) {
 function addBookToCase(element) {
     // Creating elements
     const book = document.createElement("div")
+    const removeBook = document.createElement("svg")
     const bookCover = document.createElement("div")
     const bookImage = document.createElement("img")
     const bookAuthor = document.createElement("p")
@@ -44,6 +46,7 @@ function addBookToCase(element) {
     const pageCount = document.createElement("span")
     // Classes
     book.className = "book removable"
+    removeBook.className = "remove-book"
     bookCover.className = "book-cover"
     bookImage.className = "hasCover"
     genreList.className = "genre-list"
@@ -53,12 +56,19 @@ function addBookToCase(element) {
     bookAuthor.className = "book-author"
     bookTitle.className = "book-title"
     // IDs
+    removeBook.id = element.id
     read.id = "read"
     notRead.id = "not-read"
     // Other 
     read.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Read</title><path d="M23.5,17L18.5,22L15,18.5L16.5,17L18.5,19L22,15.5L23.5,17M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,17C12.5,17 12.97,16.93 13.42,16.79C13.15,17.5 13,18.22 13,19V19.45L12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5C17,4.5 21.27,7.61 23,12C22.75,12.64 22.44,13.26 22.08,13.85C21.18,13.31 20.12,13 19,13C18.22,13 17.5,13.15 16.79,13.42C16.93,12.97 17,12.5 17,12A5,5 0 0,0 12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17Z" /></svg>'
     notRead.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Not read</title><path d="M22.54 16.88L20.41 19L22.54 21.12L21.12 22.54L19 20.41L16.88 22.54L15.47 21.12L17.59 19L15.47 16.88L16.88 15.47L19 17.59L21.12 15.47L22.54 16.88M12 9C10.34 9 9 10.34 9 12S10.34 15 12 15 15 13.66 15 12 13.66 9 12 9M12 17C9.24 17 7 14.76 7 12S9.24 7 12 7 17 9.24 17 12C17 12.5 16.9 13 16.77 13.43C17.46 13.16 18.21 13 19 13C20.12 13 21.17 13.32 22.07 13.85C22.43 13.27 22.74 12.65 23 12C21.27 7.61 17 4.5 12 4.5S2.73 7.61 1 12C2.73 16.39 7 19.5 12 19.5C12.35 19.5 12.69 19.5 13.03 19.45C13 19.3 13 19.15 13 19C13 18.21 13.16 17.46 13.43 16.77C13 16.9 12.5 17 12 17Z" /></svg>'
-    bookImage.src = element.cover
+    removeBook.innerHTML =  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Remove book</title><path d="M13 19C13 20.1 13.3 21.12 13.81 22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V13.09C19.67 13.04 19.34 13 19 13C15.69 13 13 15.69 13 19M22.54 16.88L21.12 15.47L19 17.59L16.88 15.47L15.47 16.88L17.59 19L15.47 21.12L16.88 22.54L19 20.41L21.12 22.54L22.54 21.12L20.41 19L22.54 16.88Z" /></svg>'
+    if (element.cover == undefined) {
+        bookImage.src = "default.png"
+    } 
+    else {
+        bookImage.src = element.cover
+    }
     // Appending bookCover elements
     element.genres.forEach((e)=>{
         const genreItem = document.createElement("li")
@@ -80,8 +90,18 @@ function addBookToCase(element) {
     bookCover.append(bookImage,genreList, readStatus, rating, pageCount)
     bookAuthor.append(element.author)
     bookTitle.append(element.title)
-    book.append(bookCover, bookAuthor, bookTitle)
+    book.append(removeBook ,bookCover, bookAuthor, bookTitle)
     bookcase.prepend(book)
+    // Events 
+    removeBook.addEventListener("click",(e)=>{
+        const bookID = e.currentTarget.id
+        const index = library.findIndex(item => item.id == bookID)
+        library.splice(index, 1)
+        document.querySelectorAll(".removable").forEach((e)=>{
+        bookcase.removeChild(e)
+    })
+        library.forEach(addBookToCase)
+    })
 }
 
 // Add book screen button
@@ -102,31 +122,38 @@ submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
     const formData = new FormData(form)
     const objectData = Object.fromEntries(formData)
-    if (objectData.title == "" || objectData.author == "") {
+    if (false) {
         errorMessageInfo.style.display = "inline"
         return
     }
-    else if (objectData.cover.type.slice(0,5) !== "image") {
+    else if (objectData.cover.name !== "" && objectData.cover.type.slice(0,5) !== "image") {
         errorMessageType.style.display = "inline"
         return
     }
     else {
-        document.querySelectorAll(".removable").forEach((btn)=>{
-        bookcase.removeChild(btn)
+        document.querySelectorAll(".removable").forEach((e)=>{
+        bookcase.removeChild(e)
     })
         errorMessageInfo.style.display = "none"
         errorMessageType.style.display = "none"
         bookForm.style.display = "none"
         const genreString = objectData.genres.split(",")
         // Book cover handling
-        const file = objectData.cover
-        const reader = new FileReader()
-        reader.readAsDataURL(file)
-        reader.addEventListener("load", ()=>{
-            const imageURL = reader.result
-            addBookToLibrary(objectData.title, objectData.author, objectData.pages, genreString, objectData.readS, objectData.rating, imageURL)
-            library.forEach(addBookToCase)
+        if (objectData.cover.name !== "") {
+            const file = objectData.cover
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.addEventListener("load", ()=>{
+                const imageURL = reader.result
+                addBookToLibrary(objectData.title, objectData.author, objectData.pages, genreString, objectData.readS, objectData.rating, imageURL)
+                library.forEach(addBookToCase)
+                form.reset()
         })
-        form.reset()
+        }
+        else {
+            addBookToLibrary(objectData.title, objectData.author, objectData.pages, genreString, objectData.readS, objectData.rating)
+            library.forEach(addBookToCase)
+            form.reset()
+        }
     }
 })
